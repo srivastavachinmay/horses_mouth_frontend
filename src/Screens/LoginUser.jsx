22fc5b4fe5ce019c3ab5de2f8a79ba098f51a968
Shaan3110 <hitarasigan0987654321@gmail.com>
2022-01-18 12:13:40 +0530

    login user done

--- src/Screens/LoginUser.jsx
+++ src/Screens/LoginUser.jsx
@@ -2,21 +2,39 @@ import { Button } from '@mui/material'
 import { GoogleAuthProvider,signInWithPopup } from 'firebase/auth';
 import { auth } from '../utils/firebase'
 import React,{ useState,useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const LoginUser = () => {
     const url="https://97v4h1lqe8.execute-api.ap-south-1.amazonaws.com/production";
     const [authenticate, setauthenticate] = useState(false)
     const [data, setdata] = useState({})
+    const [already, setalready] = useState(false)
+    const navigate = useNavigate()
     useEffect(() => {
-        axios.post(`${url}/user`,
-        { headers: { Authorization: `Bearer ${data.accessToken}` }})
-        .then((res)=>{
-            console.log(res)
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
+        if(authenticate)
+        {        
+            axios.post(`${url}/user`,
+            {
+                "institute": "Nil",
+                "name": `${data.displayName}`,
+                "type": "user",
+                "interests": []
+            },
+            {
+                headers: { Authorization: `Bearer ${data.accessToken}` },
+            })
+            .then((res)=>{
+                console.log(res)
+                navigate('/')
+            })
+            .catch((err)=>{
+                if(err.response.data.message==="User with this email already exists")
+                {
+                    navigate('/')
+                }
+            })
+        }
 
     }, [authenticate])
     const gauthentication = () =>{
@@ -24,7 +42,7 @@ const LoginUser = () => {
         signInWithPopup(auth,googleprovider)
         .then((res)=>{
             setdata(res.user);
-            console.log(data.accessToken)
+            // console.log(data.accessToken)
             setauthenticate(true)
         })
         .catch((err)=>{
