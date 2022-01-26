2fce31a357cb2b42c7465a45073c260a98f94405
Shaan3110 <hitarasigan0987654321@gmail.com>
2022-01-26 11:58:19 +0530

    data setup

--- src/Screens/MentorComForm.jsx
+++ src/Screens/MentorComForm.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const MentorComForm = (props) => {
   return (
     <>
-
+    {
+      console.log(props.details)
+    }
     </>
   );
 };
---
+++ src/data/data.js
@@ -0,0 +1,64 @@
+export const countries = () => {
+    let arr=[
+      'USA', 
+      'UK',
+      'China',
+      'Canada',
+      'Australia',
+      'France',
+      'Russia',
+      'Germany',
+      'Japan',
+      'Spain',
+      'Switzerland',
+      'Netherlands',
+      'Hong Kong' ,
+      'Singapore',
+      'Brazil',
+      'Taiwan',
+      'Czech Republic',
+      'Malaysia',
+      'Ireland',
+      'Poland',
+      'Chile',
+      'Norway',
+      'Finland',
+      'Sweden',
+      'New Zealand',
+      'South Korea',
+      'Italy',
+      'Denmark',
+      'Belgium',
+      'Turkey',
+      'United Arab Emirates',
+    ]
+    return arr;
+};
+
+export const degree = () => {
+  let arr=[
+    "bachelors",
+    "masters",
+    "phd"
+  ]
+  return arr;
+};
+
+export const area = () =>{
+  let arr=[
+    "STEM",
+    "Commercial-Studies",
+    "Social-Sciences",
+    "Humanities"
+  ]
+}
+
+export const relation = () =>{
+  let arr=[
+    "current",
+    "alumni"
+  ]
+}
+
+
+
